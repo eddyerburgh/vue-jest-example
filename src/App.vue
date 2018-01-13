bc339a814c7343dbcbd5b19cf8f5b53407ecff23
eddyerburgh <edward.yerburgh@gmail.com>
2018-01-13 19:36:27 +0000

    initial commit 👌

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Counter/>
+  </div>
+</template>
+
+<script>
+import Counter from './components/Counter'
+
+export default {
+  name: 'App',
+  components: {
+    Counter
+  }
+}
+</script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>counter: {{counter}}</div>
+    <button @click="counter++">Increment</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    counter: 0
+  })
+}
+</script>
